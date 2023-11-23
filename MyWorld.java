3e17587f998904b5ee848af10c97ffb074f1ba61
349749432 <349749432@gapps.yrdsb.ca>
2023-11-23 08:58:47 -0500

    add apple

--- MyWorld.java
+++ MyWorld.java
@@ -10,5 +10,8 @@ public class MyWorld extends World
         super(600, 400, 1);
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
