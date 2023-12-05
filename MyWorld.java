2ac7ec8acf6f1ea79a88ba0bfa2e001358237461
349749432 <349749432@gapps.yrdsb.ca>
2023-12-05 08:53:05 -0500

    Add score label and game over label

--- MyWorld.java
+++ MyWorld.java
@@ -6,11 +6,13 @@ public class MyWorld extends World
     
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 200, 200);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
         
         createApple();
     }
@@ -21,5 +23,20 @@ public class MyWorld extends World
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
         addObject(apple, x, y);
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }  
+    
+    public int score = 0;
+    Label scoreLabel;
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
     }
 }
